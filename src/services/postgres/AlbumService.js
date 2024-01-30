e32622d0491c1dd353a5090385e6bf5600d4f2a9
naufaladdhiya <naufal.addhiya@gmail.com>
2024-01-30 16:20:21 +0700

    Refactor getAlbumById method to include songs query

--- src/services/postgres/AlbumService.js
+++ src/services/postgres/AlbumService.js
@@ -26,14 +26,25 @@ class AlbumService {
   }
 
   async getAlbumById(id) {
-    const results = await this._pool.query({
+    const albumQuery = await this._pool.query({
       text: "SELECT * FROM albums WHERE id = $1",
       values: [id],
     });
 
-    if (!results.rows.length) throw new NotFoundError("Album tidak ditemukan");
+    const songsQuery = await this._pool.query({
+      text: "SELECT id, title, performer FROM songs WHERE album_id = $1",
+      values: [id],
+    });
 
-    return results.rows.map(mapAlbumDBToModel)[0];
+    if (!albumQuery.rows.length)
+      throw new NotFoundError("Album tidak ditemukan");
+
+    const { name, year, performer } = albumQuery.rows[0];
+
+    return {
+      ...mapAlbumDBToModel({ name, year, performer }),
+      songs: songsQuery.rows,
+    };
   }
 
   async editAlbumById(id, { name, year }) {
